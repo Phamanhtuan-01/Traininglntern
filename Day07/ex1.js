00e8b7003b45fc7c9f8c5735e86b41a895ce12c2
Phamanhtuan-01 <anhtuan01032004@gmail.com>
2025-10-28 10:08:16 +0700

    Day07

---
+++ Day07/ex1.js
@@ -0,0 +1,16 @@
+var name = "Anh Tuấn";
+console.log("Welcome to,", name);
+
+var age1 = 21
+
+console.log("Welcome to,", name, "Tuổi:", age1);
+
+
+let age = 20;
+var name = "Tuấn";
+const PI = 3.14159;
+console.log(age, name, PI);
+
+// let age = 100; => error 
+age = 100;
+console.log(age, name, PI);
